91942b3b8ddf05a78183fafc6c707499171013d9
moizh4996 <moizh4993@gmail.com>
2025-07-31 22:53:58 +0500

    chapter-1.js
    chapter-1.js

---
+++ chapter-1.js
@@ -0,0 +1,19 @@
+   
+// Question :1
+     alert("Welcome to my website! 😊");
+      
+// Question :2
+     alert("Error! Please enter a valid password");
+
+ // Question :3
+
+      alert( "Welcome to JS Land...\nHappy Coding! 😊");
+ // Question : 4
+      alert("Wellcome to JS Land...");
+      alert("Happy Coding!");
+// Question : 5
+      console.log("Hello... I can run JS through my web browser's console");
+
+
+  
+   
